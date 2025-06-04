df966ead9753e29447a1bb162e5908c265620955
dhairyasonix <sonidhairya160@gmail.com>
2025-06-04 16:21:10 +0530

    explored get post delete

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("wellcomt to Home page")
+
+
+app.use("/test", (req, res) => {
+    res.send("wellcomt to test page")
 });
 
-app.use("/about", (req, res) => {
-    res.send("wellcomt to about page")
-});
+
+app.get("/user",(req,res)=>{
+    res.send({name:"dhairya", no:"9993979695"})
+})
+app.post("/user",(req,res)=>{
+    //db save
+    res.send("succesfully saved the data")
+})
+app.delete("/user",(req,res)=>{
+    //db delete
+    res.send("succesfully deleted the data")
+})
 
 app.listen(7777, () => {
     console.log("listning to 7777")
