3b3953aafb5c73d5e301657937f5e41aab8d57b3
dhairyasonix <sonidhairya160@gmail.com>
2025-06-07 01:59:39 +0530

    validation for database

--- src/app.js
+++ src/app.js
@@ -3,21 +3,100 @@ const connectDB = require("./config/database")
 const User = require("./models/user")
 const app = express();
 
-app.post("/signup", async(req, res) => {
-    const user = new User({
-        firstName: "Dhairya",
-        lastName: "Soni",
-        emailId: "dhairya@soni.com",
-        password: "dhairya123"
-    })
-try {
-    await user.save();
-    res.send("user added sucessfully")
+app.use(express.json());
 
-} catch (error) {
-    res.status(400).send("Error saving the user",error.message)
-}
+// sign up api to create user with user data and save in db
+app.post("/signup", async (req, res) => {
+    const user = new User(req.body)
     
+    try {
+        await user.save();
+        res.send("user added sucessfully")
+
+    } catch (error) {
+        res.status(400).send("Error saving the user " + error.message)
+    }
+})
+
+// to find one by matching email id
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId
+
+    try {
+        const user = await User.findOne({ emailId: userEmail })
+        res.send(user)
+    } catch (error) {
+        res.status(404).send("something went wrong", error.message)
+    }
+})
+//delete by user id
+app.delete("/user",async(req,res)=>{
+ const id= req.body.userId
+ try {
+    await User.findByIdAndDelete(id)
+    res.send("user deleted sucessfully")
+ } catch (error) {
+    res.status(404).send("something went wrong", error.message)
+ }
+});
+
+
+// update by user id
+app.patch("/user",async(req,res,next)=>{
+const id = req.body.userId
+if(!id){return next()}
+const data = req.body
+
+try {
+  await  User.findByIdAndUpdate(id,data,{
+runValidators: true,
+
+  })
+    res.send("sucessfully updated")
+} catch (error) {
+    res.status(404).send("something went wrong " + error.message)
+}
+
+})
+app.patch("/user",async(req,res,next)=>{
+const emailId = req.body.emailId
+
+const data = req.body
+
+try {
+  await  User.updateMany({emailId: emailId},data)
+    res.send("sucessfully updated")
+} catch (error) {
+    res.status(404).send("something went wrong", error.message)
+}
+
+})
+
+
+// get data by user id
+app.get("/userid", async (req, res) => {
+    const id = req.body.id
+
+    try {
+        const user = await User.findById(id)
+        res.send(user)
+
+    } catch (error) {
+        res.status(404).send("something went wrong", error.message)
+    }
+
+})
+
+
+// Feed api to get all users
+app.get("/feed", async (req, res) => {
+    try {
+        const user = await User.find({})
+        res.send(user)
+    } catch (error) {
+        res.status(404).send("something went wrong", error.message)
+    }
+
 })
 
 connectDB().then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,53 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+    {
     firstName: {
         type: String,
+        required: true,
+        trim: true,
+        minLength:4,
+
     },
     lastName: {
         type: String,
+         trim: true,
     },
     emailId: {
         type: String,
+        required: true,
+        unique:true,
+        lowercase:true,
+         trim: true,
+        minLength:4,
     },
     password: {
         type: String,
+        required: true,
+        trim: true,
+         minLength:6,
     },
     age: {
         type: Number,
+        min:18,
+
     },
     gender: {
         type: String,
+        validate(value){
+if(!["male","female","others"].includes(value)){
+    throw new Error("Gender data is not valid")
+}
+
+        }
+    },
+    photoUrl:{
+        type: String,
+        default: "https://cdn2.iconfinder.com/data/icons/business-hr-and-recruitment/100/account_blank_face_dummy_human_mannequin_profile_user_-512.png"
+    },
+    skills:{
+        type:[String],
     }
-});
+},{ timestamps: true });
 
 module.exports = mongoose.model("User", userSchema)
