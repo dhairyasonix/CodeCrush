c1a0774314710b5dca8c88fb13a5ce6563d767f1
dhairyasonix <sonidhairya160@gmail.com>
2025-08-16 14:25:14 +0530

    modify base url  and bug: login

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -25,7 +25,7 @@ navigate("/login")
     }
 
   }
-  return ( user &&
+  return ( 
    <div className="navbar bg-base-300 shadow-sm">
   <div className="flex-1">
     <Link to="/" className="btn btn-ghost text-xl">💟CodeCrush</Link>
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:7777"
+export const TEST_URL = "http://localhost:7777"
+export const BASE_URL = "/api" // change this to test-url for dev
